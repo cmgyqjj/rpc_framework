4057de7b2892e2263b85fb33b4168b3fe06118ae
cmgyqjj <qjjypa@qq.com>
2023-07-22 23:52:20 +0800

    rpc_mini_zk新增服务注册

--- rpc_framework/rpc_mini_zk/rpc_mini_zk_framework/src/main/java/com/qjj/registry/ZkRpcRegistry.java
+++ rpc_framework/rpc_mini_zk/rpc_mini_zk_framework/src/main/java/com/qjj/registry/ZkRpcRegistry.java
@@ -2,17 +2,26 @@ package com.qjj.registry;
 
 import com.qjj.connection.ZkConnection;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author:qjj
  * @create: 2023-07-21 21:57
  * @Description: Rpc注册中心
+ *  TODO 使用统一的逐渐
  */
 @Slf4j
 public class ZkRpcRegistry implements RpcRegistry{
+
+    private volatile List<String> dataList = new ArrayList<String>();
+
 
     private ZooKeeper zooKeeper;
 
@@ -28,21 +37,29 @@ public class ZkRpcRegistry implements RpcRegistry{
 
     @Override
     public void register(RpcRegistryRequest request) throws Exception {
-
+        try {
+            String registerName = request.getServiceName() + request.getServiceVersion();
+            String path = zooKeeper.create("/registry/data", registerName.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+            log.info("create zookeeper node ({} => {})", path, registerName);
+        } catch (KeeperException | InterruptedException e) {
+            log.error("", e);
+        }
     }
 
     @Override
     public void unRegister(RpcRegistryRequest request) throws Exception {
-
+//    TODO 取消服务注册
     }
 
     @Override
     public RpcRegistryRequest discovery(String serviceName, int invokerHashCode) throws Exception {
+//        TODO 服务发现
+
         return null;
     }
 
     @Override
     public void destroy() throws IOException {
-
+//        TODO 销毁
     }
 }
